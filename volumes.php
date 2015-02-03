087b5971a412a9b879354b08d7f3c1e8f5128ee6
Alexander Hank <mail@alexander-hank.de>
2015-02-03 15:59:42 +0100

    Improve listing of existing volumes

--- volumes.php
+++ volumes.php
@@ -5,25 +5,42 @@
     require 'views/overview/menu.html';
 
     try {
-        if (file_exists($proc_volumes)) {
-            $a_volumes = get_file_cat($proc_volumes);
-            if ($a_volumes == "error") {
-                throw new Exception("Error - Could not create list of volumes");
-            } else {
-                $name = get_data_regex($a_volumes, "/name:(.*)/");
-                $tid = get_data_regex($a_volumes, "/tid:([0-9].*?)/");
-                $paths = get_data_regex($a_volumes, "/path:(.*)/");
-                $lun = get_data_regex($a_volumes, "/lun:([0-9].*?)/");
-                $state = get_data_regex($a_volumes, "/state:([0-9].*?)/");
-                $type = get_data_regex($a_volumes, "/iotype:([a-z].*?) /");
-                $blocks = get_data_regex($a_volumes, "/blocks:([0-9].*?) /");
-                $size = get_data_regex($a_volumes, "/blocksize:([0-9].*?) /");
+        // Check if service is running and abort if not
+        check_service_status();
 
-                require 'views/volumes/output.html';
-            }
-        } else {
-            throw new Exception("Error - The file $proc_volumes was not found");
+        // Read contents of file $proc_volumes in var
+        $volumes = file_get_contents($proc_volumes);
+
+        if (empty($volumes)) {
+            throw new exception("Error - Could not create list of volumes");
         }
+
+        for ($b=0; $b < substr_count($volumes, "\n")/2; $b++) {
+            preg_match_all("/name:(.*)/", $volumes, $result);
+            $data[$b][0] = $result[1][$b];
+
+            preg_match_all("/tid:([0-9].*?)/", $volumes, $result);
+            $data[$b][1] = $result[1][$b];
+
+            preg_match_all("/path:(.*)/", $volumes, $result);
+            $data[$b][2] = $result[1][$b];
+
+            preg_match_all("/lun:([0-9].*?)/", $volumes, $result);
+            $data[$b][3] = $result[1][$b];
+
+            preg_match_all("/state:([0-9].*?)/", $volumes, $result);
+            $data[$b][4] = $result[1][$b];
+
+            preg_match_all("/iotype:([a-z].*?) /", $volumes, $result);
+            $data[$b][5] = $result[1][$b];
+
+            preg_match_all("/blocks:([0-9].*?) /", $volumes, $result);
+            $data[$b][6] = $result[1][$b];
+
+            preg_match_all("/blocksize:([0-9].*?) /", $volumes, $result);
+            $data[$b][7] = $result[1][$b];
+        }
+        require 'views/volumes/output.html';
     }  catch (Exception $e) {
         $error = $e->getMessage();
         require 'views/error.html';
