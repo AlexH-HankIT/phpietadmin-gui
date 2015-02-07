9289d2266fe9cb564303b9ca389e35ff47778ef5
Alexander Hank <mail@alexander-hank.de>
2015-02-07 23:31:06 +0100

    various improvements and fixes

--- mondata.php
+++ mondata.php
@@ -7,7 +7,10 @@
     $out = "<p>Load: " . $load[0] . " " . $load[1] . " " . $load[2] . "</p>";
 
     // Get uptime
-    $uptime = shell_exec("uptime -p");
+    $data = shell_exec('uptime');
+    $uptime = explode(' up ', $data);
+    $uptime = explode(',', $uptime[1]);
+    $uptime = $uptime[0].', '.$uptime[1];
 
     require "$_SERVER[DOCUMENT_ROOT]/phpietadmin/views/mondata.html";
 ?>
