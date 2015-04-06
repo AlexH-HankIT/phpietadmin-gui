abf5d86615d40258639803a04d5cfa1f887f9aa5
Alexander Hank <mail@alexander-hank.de>
2015-04-06 03:22:19 +0200

    Create javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-document.getElementById("nav01").innerHTML =
-    "<ul id='menu'>" +
+function showMenu() {
+    document.getElementById("nav01").innerHTML =
+        "<ul id='menu'>" +
         "<li><a href='/phpietadmin/index.php'>Home</a></li>" +
         "<li><a href='/phpietadmin/overview.php'>Overview</a></li>" +
         "<li><a href='/phpietadmin/targets/targets.php'>Targets</a></li>" +
         "<li><a href='/phpietadmin/allow/initiators/allow.php'>Allow</a></li>" +
         "<li><a href='/phpietadmin/lvm/lvm.php'>LVM</a></li>" +
         "<li><a href='/phpietadmin/service.php'>Service</a></li>" +
-    "</ul>";
+        "</ul>";
+}
+
+function showValue(newValue, freesize) {
+    if(newValue <= freesize) {
+        document.getElementById("rangeinput").value=newValue;
+        document.getElementById("range").innerHTML=newValue;
+    } else {
+        alert("Error - The volume group has only " + freesize + "G space left");
+        updateTextInput(freesize);
+        document.getElementById("range").innerHTML=freesize;
+    }
+}
+
+function updateTextInput(newValue) {
+    document.getElementById("sizefield").value=newValue;
+}
