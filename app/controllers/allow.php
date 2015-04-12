25f7459210f5156339d623649a98d2fb5997f0c9
Alexander Hank <mail@alexander-hank.de>
2015-04-12 04:22:15 +0200

    Add allow

---
+++ app/controllers/allow.php
@@ -0,0 +1,82 @@
+<?php
+    class allow extends Controller {
+        public function index() {
+            $this->view('header');
+            $this->view('menu');
+            $this->view('allow/index');
+            $this->view('footer');
+        }
+
+        public function addinitiator() {
+            $database = $this->model('Database');
+            $ietpermissions = $this->model('Ietpermissions');
+            $ietvolume = $this->model('IetVolumes');
+
+            $this->view('header');
+            $this->view('menu');
+            $this->view('allow/index');
+
+            $data = $ietvolume->getProcVolumes();
+
+            if ($data == 2) {
+                $this->view('message', "Error - No targets found");
+            } elseif ($data == 1) {
+                $this->view('message', "Error - The iet volumes file under /proc doesn't exist!");
+            } else {
+                preg_match_all("/name:(.*)/", $data, $a_name);
+                $a_name = $a_name[1];
+
+                $a_initiators = $ietpermissions->get_allow($database->getConfig('ietd_init_allow'));
+                $a_name = $ietpermissions->get_targets_without_rules($a_initiators, $a_name);
+
+                if ($a_name == 3) {
+                    $this->view('message', "Error - Rules for all targets are allready set!");
+                } else {
+                    if (empty($_POST['IQNs'])) {
+                        $this->view('allow/add', $a_name);
+                    } else {
+                        $return = $ietpermissions->write_allow_rule($_POST['IQNs'], $a_name);
+
+                        if ($return == 1) {
+                            $this->view('message', "Error - The iet initiator allow file was not found or is read only!");
+                        } else {
+                            $this->view('message', "Success");
+                        }
+                    }
+                }
+            }
+            $this->view('footer');
+        }
+
+        public function deleteinitiator() {
+            $database = $this->model('Database');
+            $ietpermissions = $this->model('Ietpermissions');
+
+            $this->view('header');
+            $this->view('menu');
+            $this->view('allow/index');
+
+            $a_initiators = $ietpermissions->get_allow($database->getConfig('ietd_init_allow'));
+
+            if ($a_initiators == 3) {
+                $this->view('message', "Error - No allow rules set");
+            } elseif ($a_initiators == 1) {
+                $this->view('message', "Error - The iet initiator allow file was not found or is read only!");
+            } else {
+                $a_initiators2 = $ietpermissions->get_initiator_array($a_initiators);
+
+                if (empty($_POST['IQNs2'])) {
+                    $this->view('allow/delete', $a_initiators2);
+                } else {
+                    $return = $ietpermissions->delete_allow_rule($a_initiators2);
+                    if ($return == 1) {
+                        $this->view('message', "Error - The iet initiator allow file was not found or is read only!");
+                    } else {
+                        $this->view('message', "Success");
+                    }
+                }
+            }
+            $this->view('footer');
+        }
+    }
+?>
