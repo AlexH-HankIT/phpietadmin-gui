4d152d4abf851297cb3cf93d755ce0250700e7b9
Alexander Hank <mail@alexander-hank.de>
2015-04-19 19:35:34 +0200

    align center

--- app/views/home.php
+++ app/views/home.php
@@ -1,10 +1,10 @@
 <div class = "container">
-    <h1>Welcome to phpietadmin!</h1>
+    <h1><p align="center">Welcome to phpietadmin!</p></h1>
     <br />
     <div class = "jumbotron">
         <p>Thats what i can do:</p>
         <br />
-        <p><b>Overview</b> gives information about disks, volumes, allow rules and all kind of lvm volumes</p>
+        <p ><b>Overview</b> gives information about disks, volumes, allow rules and all kind of lvm volumes</p>
         <p><b>Targets</b> allows to add or delete a target</p>
         <p><b>Allow</b> allows to create and delete allow rules for targets</p>
         <p><b>LVM</b> allows to create or delete logical volumes</p>
