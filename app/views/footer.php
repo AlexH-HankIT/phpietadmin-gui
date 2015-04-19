5f3de8036d6756bec08a5e78fc4cae474da103f0
Alexander Hank <mail@alexander-hank.de>
2015-04-19 20:04:15 +0200

    Add javascript to footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -5,7 +5,9 @@
         <a class = "navbar-btn btn-success btn pull-left">ietd running</a>-->
     </div>
 </div>
-
+<script type="text/javascript" src=/phpietadminv02/js/jquery-2.1.3.min.js></script>
+<script type="text/javascript" src=/phpietadminv02/js/bootstrap.js></script>
+<script type="text/javascript" src=/phpietadminv02/js/scripts.js></script>
 <?php //require "$_SERVER[DOCUMENT_ROOT]/phpietadmin/mondata.php"; ?>
 </body>
 </html>
--- app/views/header.php
+++ app/views/header.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/phpietadminv02/css/bootstrap.css" type="text/css" rel="stylesheet">
     <link href="/phpietadminv02/css/my.css" type="text/css" rel="stylesheet">
-    <script type="text/javascript" src=/phpietadminv02/js/jquery-2.1.3.min.js></script>
-    <script type="text/javascript" src=/phpietadminv02/js/bootstrap.js></script>
-    <script type="text/javascript" src=/phpietadminv02/js/scripts.js></script>
 </head>
 <title>phpietadmin</title>
 <body>
