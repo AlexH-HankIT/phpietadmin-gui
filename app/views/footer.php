dee35c46de71eddbd5eb0db96740b11ab6eeae4f
Alexander Hank <mail@alexander-hank.de>
2015-04-19 20:13:40 +0200

    Add link to footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,6 +1,6 @@
 <div class = "navbar navbar-default navbar-fixed-bottom">
     <div class = "container">
-        <p class = "navbar-text pull-right">Created by MrCrankHank</p>
+        <a href = 'https://github.com/MrCrankHank' target = 'new' class = "navbar-text pull-right">Created by MrCrankHank</a>
         <!--<a class = "navbar-btn btn-danger btn pull-left">ietd not running</a>
         <a class = "navbar-btn btn-success btn pull-left">ietd running</a>-->
     </div>
