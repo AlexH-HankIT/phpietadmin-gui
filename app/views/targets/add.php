3643575176b8ce77bf0b62b9ec132e652937c930
Alexander Hank <mail@alexander-hank.de>
2015-04-20 23:48:58 +0200

    Add row

--- app/views/targets/add.php
+++ app/views/targets/add.php
@@ -9,23 +9,26 @@
                     <?php for ($i = 0; $i < count($data); $i++) { echo '<option value=' . ($i+1) . '>' . $data[$i] . '</option>'; } ?>
                 </select>
             </p>
-
-            <p>
-                Type:
-            <select name="type" id="type" multiple class="form-control">
-                <option value="fileio">fileio</option>
-                <option selected value="blockio" >blockio</option>
-            </select>
-            </p>
-
-            <p>
-                Mode:
-                <select name="mode" id="mode" multiple class="form-control">
-                    <option selected value="wt" >Write through</option>
-                    <option value="ro" >Read only</option>
-                </select>
-            </p>
-
+            <div class = "row">
+                <div class = "col-md-6">
+                    <p>
+                        Type:
+                        <select name="type" id="type" multiple class="form-control">
+                            <option value="fileio">fileio</option>
+                            <option selected value="blockio" >blockio</option>
+                        </select>
+                    </p>
+                </div>
+                <div class = "col-md-6">
+                    <p>
+                        Mode:
+                        <select name="mode" id="mode" multiple class="form-control">
+                            <option selected value="wt" >Write through</option>
+                            <option value="ro" >Read only</option>
+                        </select>
+                    </p>
+                </div>
+            </div>
             <p><input class="btn btn-primary" type="submit" value="Create" onclick="return validatetargetadd();"/></p>
         </form>
     </div>
