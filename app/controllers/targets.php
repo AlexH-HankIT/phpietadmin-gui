cb3db4e827a3f5e26a42dc30e4a8af33ff713094
Alexander Hank <mail@alexander-hank.de>
2015-05-06 19:57:34 +0200

    Delete obsolete code

--- app/controllers/targets.php
+++ app/controllers/targets.php
@@ -31,27 +31,17 @@
             $this->view('menu');
 
             if (isset($_POST['name'])) {
-                $data = $lvm->get_all_logical_volumes();
-                if ($data == 3) {
-                    $this->view('message', "Error - No logical volumes found!");
+                $return = $ietadd->check_target_name_already_in_use($_POST['name']);
+                if ($return == 4) {
+                    $this->view('message', "Error - The name " . $_POST['name'] . " is already taken!");
                 } else {
-                    $return = $ietadd->check_target_name_already_in_use($_POST['name']);
-                    if ($return == 4) {
-                        $this->view('message', "Error - The name " . $_POST['name'] . " is already taken!");
+                    $return = $std->exec_and_return($database->getConfig('sudo') . " " . $database->getConfig('ietadm') . " --op new --tid=0 --params Name=" . $_POST['name']);
+                    if ($return != 0) {
+                        $this->view('message', "Error - Could not add target " . $_POST['name'] . ". Server said: $return[0]");
                     } else {
-                        $data = $ietadd->get_unused_volumes($data[2]);
-                        if (empty($data)) {
-                            $this->view('message', "Error - No logical volumes found!");
-                        } else {
-                            $return = $std->exec_and_return($database->getConfig('sudo') . " " . $database->getConfig('ietadm') . " --op new --tid=0 --params Name=" . $_POST['name']);
-                            if ($return != 0) {
-                                $this->view('message', "Error - Could not add target " . $_POST['name'] . ". Server said: $return[0]");
-                            } else {
-                                $line = "Target " . $_POST['name'] . "\n";
-                                $std->add_line_to_file($line, $database->getConfig('ietd_config_file'));
-                                $this->view('message', "Success");
-                            }
-                        }
+                        $line = "Target " . $_POST['name'] . "\n";
+                        $std->add_line_to_file($line, $database->getConfig('ietd_config_file'));
+                        $this->view('message', "Success");
                     }
                 }
             } else {
@@ -82,13 +72,13 @@
                 if ($return != 0) {
                     $this->view('message', "Error - Could not add lun to target " . $_POST['target'] . " Server said:" . $return[0]);
                 } else {
-                    // add lun to config file here
                     $line = "Lun " . $LUN . " Type=" . $_POST['type'] . ",IOMode=" . $_POST['mode'] . ",Path=" . $_POST['path'];
                     $std->addlineafterpattern("Target " . $_POST['target'], $database->getConfig('ietd_config_file'), $line);
+                    $this->view('message', "Success");
                 }
 
             } else if (!empty($_POST['target']) && !empty($_POST['type']) && !empty($_POST['mode']) && !empty($_POST['pathtoblockdevice'])) {
-                // handle manuel selection here
+                // handle manual selection here
             } else {
                 if ($data == 3) {
                     $this->view('message', "Error - No logical volumes found!");
@@ -113,77 +103,18 @@
             $this->view('footer', $data);
         }
 
+        public function deletelun() {
+            $std = $this->model('Std');
 
-        /*function add() {
-            $session = $this->model('Session');
-            $session->setUsername($_SESSION['username']);
-            $session->setPassword($_SESSION['password']);
+            $this->view('header');
+            $this->view('menu');
 
-            if ($session->check()) {
-                $lvm = $this->model('Lvmdisplay');
-                $database = $this->model('Database');
-                $ietadd = $this->model('Ietaddtarget');
-                $std = $this->model('Std');
 
-                $this->view('header');
-                $this->view('menu');
-                if (isset($_POST['name']) && isset($_POST['path']) && isset($_POST['type'])) {
-                    $NAME = $_POST['name'];
-                    $TYPE = $_POST['type'];
-                    $MODE = $_POST['mode'];
 
-                    $data = $lvm->get_all_logical_volumes();
-                    if ($data == 3) {
-                        $this->view('message', "Error - No logical volumes found!");
-                    } else {
-                        $return = $ietadd->check_target_name_already_in_use($NAME);
-                        if ($return == 4) {
-                            $this->view('message', "Error - The name $NAME is already taken!");
-                        } else {
-                            $data = $ietadd->get_unused_volumes($data[2]);
-                            if (empty($data)) {
-                                $this->view('message', "Error - No logical volumes found!");
-                            } else {
-                                $return = $std->exec_and_return($database->getConfig('sudo') . " " . $database->getConfig('ietadm') . " --op new --tid=0 --params Name=" . $database->getConfig('iqn') . ":" . $NAME);
-                                if ($return != 0) {
-                                    $this->view('message', "Error - Could not add target $NAME. Server said: $return[0]");
-                                } else {
-                                    $TID = $ietadd->get_tid($NAME);
-                                    $return = $std->exec_and_return($database->getConfig('sudo') . " " . $database->getConfig('ietadm') . " --op new --tid=" . $TID . " --lun=0 --params Path=" . $_POST['path'] . ",Type=" . $TYPE . ",IOMode=" . $MODE);
-                                    if ($return != 0) {
-                                        $this->view('message', "Error - Could not add lun. Server said: $return[0]");
-                                    } else {
-                                        $return = $ietadd->write_target_and_lun($NAME, $_POST['path'], $TYPE, $MODE);
-                                        if ($return == 6) {
-                                            $this->view('message', "Error - Could not write into the ietd config file!");
-                                        } elseif ($return == 0) {
-                                            $this->view('message', "Success");
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    $data = $lvm->get_all_logical_volumes();
 
-                    if ($data == 3) {
-                        $this->view('message', "Error - No logical volumes found!");
-                    } else {
-                        $data = $ietadd->get_unused_volumes($data[2]);
-                        if (empty($data)) {
-                            $this->view('message', "Error - No logical volumes available!");
-                        } else {
-                            $this->view('targets/add', $data);
-                        }
-                    }
-                }
-                $data = $std->get_service_status();
-                $this->view('footer', $data);
-            } else {
-                header("Location: /phpietadmin/auth/login");
-            }
-        }*/
+            $data = $std->get_service_status();
+            $this->view('footer', $data);
+        }
 
         public function delete() {
             $ietdelete = $this->model('Ietdeletetarget');
