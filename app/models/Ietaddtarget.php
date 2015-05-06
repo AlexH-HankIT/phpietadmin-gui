3bc039a3ee5d2d8cbb75d54a2b7be130f55e4752
MrCrankHank <mail@alexander-hank.de>
2015-05-06 16:25:41 +0200

    fix bug in get_tid function

--- app/models/Ietaddtarget.php
+++ app/models/Ietaddtarget.php
@@ -47,7 +47,8 @@
             $volumes = file_get_contents($database->getConfig('proc_volumes'));
             preg_match_all("/tid:([0-9].*?) /", $volumes, $a_tid);
             preg_match_all("/name:(.*)/", $volumes, $a_name);
-            $key = array_search($NAME, $a_name);
+
+            $key = array_search($NAME, $a_name[1]);
             return $a_tid[1][$key];
         }
 
@@ -62,6 +63,108 @@
             } else {
                 return 0;
             }
+        }
+
+        public function get_targets() {
+            require_once 'Database.php';
+            $database = new Database();
+
+            $volumes = file_get_contents($database->getConfig('proc_volumes'));
+
+            if (!empty($volumes)) {
+                preg_match_all("/name:(.*)/", $volumes, $a_name);
+                for ($i=0; $i < count($a_name[1]); $i++) {
+                    $data[$i] = $a_name[1][$i];
+                }
+                return $data;
+            } else {
+                return 3;
+            }
+        }
+
+        public function get_lun($PATH) {
+            require_once 'Database.php';
+            $database = new Database();
+
+            $volumes = file($database->getConfig('proc_volumes'));
+
+            $key = array_search($volumes, $PATH);
+            preg_match("/lun:([0-9].*?) /", $volumes[$key], $a_lun);
+
+            return $a_lun;
+        }
+
+        public function add_lun_to_config_file($NAME, $BLOCKDEVICE, $TYPE, $MODE) {
+
+        }
+
+        public function get_next_lun($TARGET) {
+            require_once 'Database.php';
+            $database = new Database();
+
+            $data = file($database->getConfig('proc_volumes'));
+
+            // Replace all newlines with spaces
+            $data = trim(preg_replace('/\s\s+/', ' ', $data));
+
+            // Explode array by 'tid'
+            $data = array_values(array_filter(explode('tid:', $data)));
+
+            // Explode arrays by space
+            $counter=0;
+            foreach ($data as $value) {
+                $data2[$counter] = explode(' ', $value);
+                $counter++;
+            }
+
+            $counter=0;
+            foreach ($data2 as $value) {
+                // All arrays with less than two rows don't contain interesting data
+                if (count($value) > 2) {
+                    $volumes[$counter] = $value;
+                }
+                $counter++;
+            }
+
+            // Empty means, target has no luns, therefore we create the first lun with id 0
+            if (empty($volumes)) {
+                return 0;
+            }
+
+            $volumes = array_values($volumes);
+
+            $counter=0;
+            foreach ($volumes as $value) {
+                preg_match("/name:(.*)/", $value[1], $result);
+                $var[$counter][0]['name'] = $result[1];
+
+                for ($i=2; $i < count($value); $i=$i+7) {
+                    preg_match("/lun:([0-9].*)/", $value[$i], $result);
+                    $var[$counter][$counter+$i]['lun'] = $result[1];
+                }
+
+                $counter++;
+            }
+
+            // Correct index
+            for ($i=0; $i < count($var); $i++) {
+                $var[$i] = array_values($var[$i]);
+            }
+
+            echo "<pre>";
+            print_r($var);
+            echo "</pre>";
+
+            for ($i=0; $i < count($var); $i++) {
+                if ($var[$i][0]['name'] === $TARGET) {
+                    for ($b=1; $b < count($var[$i]); $b++) {
+                        $highestlun = $var[$i][$b]['lun'];
+                    }
+                }
+            }
+
+            // Add 1 to get the next free lun
+            return $highestlun + 1;
         }
     }
 ?>
