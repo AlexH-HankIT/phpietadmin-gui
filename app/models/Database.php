0a4beb4b950c93f9830fda0b3c20c9063873e2b9
MrCrankHank <mail@alexander-hank.de>
2015-05-07 16:22:03 +0200

    Add closing function

--- app/models/Database.php
+++ app/models/Database.php
@@ -1,16 +1,20 @@
 <?php
     class Database extends SQLite3 {
-        function __construct() {
+        public function __construct() {
             $dbpath = "/usr/share/phpietadmin/app/config.db";
             $this->open($dbpath);
         }
 
-        function getConfig($option) {
+        public function getConfig($option) {
             $data = $this->prepare('SELECT value from config where option=:option');
             $data->bindValue('option', $option, SQLITE3_TEXT );
             $result = $data->execute();
             $result = $result->fetchArray();
             return $result['value'];
+        }
+
+        public function __destruct() {
+            $this->close();
         }
     }
 ?>
