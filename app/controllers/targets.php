aa11bf039a0d0508f9becb331aa7017d4bbf4311
Alexander Hank <mail@alexander-hank.de>
2015-05-10 22:27:59 +0200

    Create global objects for models

--- app/controllers/targets.php
+++ app/controllers/targets.php
@@ -1,77 +1,86 @@
 <?php
     class targets extends controller {
+        // Define global vars
+        var $database;
+        var $ietadd;
+        var $std;
+        var $session;
+        var $lvm;
+
         public function __construct() {
-            $session = $this->model('Session');
-            $session->setUsername($_SESSION['username']);
-            $session->setPassword($_SESSION['password']);
+            $this->create_models();
+
+            $this->session->setUsername($_SESSION['username']);
+            $this->session->setPassword($_SESSION['password']);
 
             // Check if user is logged in
-            if (!$session->check()) {
+            if (!$this->session->check()) {
                 header("Location: /phpietadmin/auth/login");
                 // Die in case browser ignores header redirect
                 die();
+            } else {
+                // Check if ietd service is running
+                $data = $this->std->get_service_status();
+                if ($data[1] ==! 0) {
+                    $this->view('header');
+                    $this->view('menu');
+                    $this->view('message', "Error - ietd service is not running!");
+                    $this->view('footer', $data);
+                    die();
+                }
             }
         }
 
-        public function index() {
-            $std = $this->model('Std');
-            $this->view('header');
-            $this->view('menu');
-            $data = $std->get_service_status();
-            $this->view('footer', $data);
+        private function create_models() {
+            // Create models used in this controller
+            $this->session = $this->model('Session');
+            $this->database = $this->model('Database');
+            $this->ietadd = $this->model('Ietaddtarget');
+            $this->std = $this->model('Std');
+            $this->lvm = $this->model('Lvmdisplay');
         }
 
         public function addtarget() {
-            $database = $this->model('Database');
-            $ietadd = $this->model('Ietaddtarget');
-            $std = $this->model('Std');
-
             if (isset($_POST['name'])) {
-                $return = $ietadd->check_target_name_already_in_use($_POST['name']);
+                $return = $this->ietadd->check_target_name_already_in_use($_POST['name']);
                 if ($return == 4) {
                     $this->view('message', "Error - The name " . $_POST['name'] . " is already taken!");
                 } else {
-                    $return = $std->exec_and_return($database->get_config('sudo') . " " . $database->get_config('ietadm') . " --op new --tid=0 --params Name=" . $_POST['name']);
+                    $return = $this->std->exec_and_return($this->database->get_config('sudo') . " " . $this->database->get_config('ietadm') . " --op new --tid=0 --params Name=" . $_POST['name']);
                     if ($return != 0) {
                         $this->view('message', "Error - Could not add target " . $_POST['name'] . ". Server said: $return[0]");
                     } else {
                         $line = "Target " . $_POST['name'] . "\n";
-                        $std->add_line_to_file($line, $database->get_config('ietd_config_file'));
+                        $this->std->add_line_to_file($line, $this->database->get_config('ietd_config_file'));
                         $this->view('message', "Success");
                     }
                 }
             } else {
                 $this->view('header');
                 $this->view('menu');
-                $this->view('targets/addtarget', $database->get_config('iqn') . ":");
-                $data = $std->get_service_status();
-                $this->view('footer', $data);
+                $this->view('targets/addtarget', $this->database->get_config('iqn') . ":");
+                $this->view('footer', $this->std->get_service_status());
             }
 
 
         }
 
         public function maplun() {
-            $lvm = $this->model('Lvmdisplay');
-            $database = $this->model('Database');
-            $ietadd = $this->model('Ietaddtarget');
-            $std = $this->model('Std');
-
-            $data = $lvm->get_all_logical_volumes();
+            $data = $this->lvm->get_all_logical_volumes();
 
             if (!empty($_POST['target']) && !empty($_POST['type']) && !empty($_POST['mode']) && !empty($_POST['path'])) {
-                $TID = $ietadd->get_tid($_POST['target']);
-                $LUN = $ietadd->get_next_lun($_POST['target']);
-                $return = $ietadd->check_path_already_in_use($_POST['path']);
+                $TID = $this->ietadd->get_tid($_POST['target']);
+                $LUN = $this->ietadd->get_next_lun($_POST['target']);
+                $return = $this->ietadd->check_path_already_in_use($_POST['path']);
                 if ($return != 0) {
                     $this->view('message', "Error - The path " . $_POST['path'] . " is already in use");
                 } else {
-                    $return = $std->exec_and_return($database->get_config('sudo') . " " . $database->get_config('ietadm') . " --op new --tid=" . $TID . " --lun=" . $LUN . " --params Path=" . $_POST['path'] . ",Type=" . $_POST['type'] . ",IOMode=" . $_POST['mode']);
+                    $return = $this->std->exec_and_return($this->database->get_config('sudo') . " " . $this->database->get_config('ietadm') . " --op new --tid=" . $TID . " --lun=" . $LUN . " --params Path=" . $_POST['path'] . ",Type=" . $_POST['type'] . ",IOMode=" . $_POST['mode']);
                     if ($return != 0) {
                         $this->view('message', "Error - Could not add lun to target " . $_POST['target'] . " Server said:" . $return[0]);
                     } else {
                         $line = "Lun " . $LUN . " Type=" . $_POST['type'] . ",IOMode=" . $_POST['mode'] . ",Path=" . $_POST['path'] . "\n";
-                        $std->addlineafterpattern("Target " . $_POST['target'], $database->get_config('ietd_config_file'), $line);
+                        $this->std->addlineafterpattern("Target " . $_POST['target'], $this->database->get_config('ietd_config_file'), $line);
                         $this->view('message', "Success");
                     }
                 }
@@ -84,34 +93,29 @@
                 if ($data == 3) {
                     $this->view('message', "Error - No logical volumes found!");
                 } else {
-                    $data = $ietadd->get_unused_volumes($data[2]);
+                    $data = $this->ietadd->get_unused_volumes($data[2]);
                     if (empty($data)) {
                         $this->view('message', "Error - No logical volumes available!");
                     } else {
                         $data['logicalvolumes'] = $data;
-                        $data['targets'] = $ietadd->get_targets();
+                        $data['targets'] = $this->ietadd->get_targets();
 
                         if ($data == 4) {
                             $this->view('message', "Error - No targets found");
-                        } else {            $this->view('header');
-
+                        } else {
+                            $this->view('header');
                             $this->view('targets/maplun', $data);
-
                         }
                     }
                 }
-                $data = $std->get_service_status();
-                $this->view('footer', $data);
+                $this->view('footer', $this->std->get_service_status());
             }
         }
 
         public function deletelun() {
-            $std = $this->model('Std');
-            $ietadd = $this->model('Ietaddtarget');
-
-            if (isset($_POST['iqn'])) {
+            if (isset($_POST['iqn']) && !isset($_POST['lun'])) {
                 // Get luns for selected target
-                $data = $ietadd->get_targets_with_lun();
+                $data = $this->ietadd->get_targets_with_lun();
 
                 foreach ($data as $value) {
                     if (strcmp($value[0]['name'], $_POST['iqn']) === 0) {
@@ -124,16 +128,27 @@
 
                 // Display page for ajax request
                 $this->view('targets/deletelun02', $paths);
+            } else if (isset($_POST['iqn']) && isset($_POST['lun']) && isset($_POST['path'])) {
+                // Delete lun from daemon
+                $tid = $this->ietadd->get_tid($_POST['iqn']);
+                $return = $this->std->exec_and_return($this->database->get_config('sudo') . " " . $this->database->get_config('ietadm') . " --op delete --tid=" . $tid . " --lun=" . $_POST['lun']);
+
+                if ($return != 0) {
+                    $this->view('message', "Error - Could not delete lun " . $_POST['lun'] . " from target " . $_POST['iqn'] . " Server said:" . $return[0]);
+                } else {
+                    // $_POST['type']
+                    // $_POST['mode']
+
+                    $line = "Lun " . $_POST['lun'] . " Type=" . $_POST['type'] . ",IOMode=" . $_POST['mode'] . ",Path=" . $_POST['path'] . "\n";;
+                    $this->std->deletelineinfile($this->database->get_config('ietd_config_file'), $line);
+
+                    $this->view('message', "Success");
+                }
             } else {
                 $this->view('header');
                 $this->view('menu');
 
-                $data = $ietadd->get_targets_with_lun();
-
-                //echo "<pre>";
-                //print_r($data);
-                //echo "</pre>";
-
+                $data = $this->ietadd->get_targets_with_lun();
 
                 // Extract target names
                 $counter=0;
@@ -144,33 +159,28 @@
 
                 $this->view('targets/deletelun01', $targets);
 
-                $data = $std->get_service_status();
-                $this->view('footer', $data);
+                $this->view('footer', $this->std->get_service_status());
             }
         }
 
         public function delete() {
-            $ietdelete = $this->model('Ietdeletetarget');
-            $database = $this->model('Database');
-            $std = $this->model('Std');
             $this->view('header');
             $this->view('menu');
             if (isset($_POST['IQN'])) {
-                $data = $ietdelete->parse_data($_POST['IQN']);
-                $command = $database->get_config('sudo') . " " . $database->get_config('ietadm') . " --op delete --tid=" . $data[0];
-                $std->exec_and_return($command);
-                $ietdelete->delete_from_config_file($data);
+                $data = $this->ietdelete->parse_data($_POST['IQN']);
+                $command = $this->database->get_config('sudo') . " " . $this->database->get_config('ietadm') . " --op delete --tid=" . $data[0];
+                $this->std->exec_and_return($command);
+                $this->ietdelete->delete_from_config_file($data);
                 $this->view('message', "Success");
             } else {
-                $data = $ietdelete->get_names();
+                $data = $this->ietdelete->get_names();
                 if ($data == 2) {
                     $this->view('message', "Error - No targets found");
                 } else {
                     $this->view('targets/delete', $data);
                 }
             }
-            $data = $std->get_service_status();
-            $this->view('footer', $data);
+            $this->view('footer', $this->std->get_service_status());
         }
     }
 ?>
