08dcb0bb0691d9353387e8161a6c4fc29c55f271
Alexander Hank <mail@alexander-hank.de>
2015-05-23 02:05:26 +0200

    add check_loggedin function

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -25,6 +25,40 @@
             $this->ietpermissions = $this->model('Ietpermissions');
         }
 
+        public function check_loggedin($session) {
+            $session->setUsername($_SESSION['username']);
+            $session->setPassword($_SESSION['password']);
+
+            // Check if user is logged in
+            if (!$session->check()) {
+                header("Location: /phpietadmin/auth/login");
+                // Die in case browser ignores header redirect
+                die();
+            }
+        }
+
+        public function check_logged_in_service_running($session) {
+            $session->setUsername($_SESSION['username']);
+            $session->setPassword($_SESSION['password']);
+
+            // Check if user is logged in
+            if (!$session->check()) {
+                header("Location: /phpietadmin/auth/login");
+                // Die in case browser ignores header redirect
+                die();
+            } else {
+                // Check if ietd service is running
+                $data = $this->std->get_service_status();
+                if ($data[1] ==! 0) {
+                    $this->view('header');
+                    $this->view('menu');
+                    $this->view('message', "Error - ietd service is not running!");
+                    $this->view('footer', $data);
+                    die();
+                }
+            }
+        }
+
         public function model($model) {
             require_once '../app/models/' . $model . '.php';
             return new $model();
