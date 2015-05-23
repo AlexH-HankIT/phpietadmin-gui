d1fa01a7430421c1713b87539b779203528eeb31
Alexander Hank <mail@alexander-hank.de>
2015-05-23 02:15:13 +0200

    Add jquery plugins

--- app/views/header.php
+++ app/views/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/phpietadmin/css/bootstrap.css" type="text/css" rel="stylesheet">
+    <link href="/phpietadmin/css/jquery.qtip.css" type="text/css" rel="stylesheet">
     <link href="/phpietadmin/css/my.css" type="text/css" rel="stylesheet">
     <?php if ($data == "login") { ?>
         <link href="/phpietadmin/css/login.css" type="text/css" rel="stylesheet">
@@ -12,6 +13,8 @@
     <script type="text/javascript" src=/phpietadmin/js/jquery-2.1.3.min.js></script>
     <script type="text/javascript" src=/phpietadmin/js/bootstrap.js></script>
     <!--<script type="text/javascript" src=/phpietadmin/js/jquery.input-ip-address-control.js></script>-->
+    <script type="text/javascript" src=/phpietadmin/js/jquery.qtip.js></script>
+    <script type="text/javascript" src=/phpietadmin/js/imagesloaded.pkg.min.js></script>
     <script type="text/javascript" src=/phpietadmin/js/myfunctions.js></script>
     <script type="text/javascript" src=/phpietadmin/js/myscripts.js></script>
 </head>
