4cd41cbfae20c5af06484b5a8417ab9006c17b27
MrCrankHank <mail@alexander-hank.de>
2015-07-12 22:08:10 +0200

    improve responsive footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,17 +1,33 @@
-<div id="footer" class = 'navbar navbar-default navbar-fixed-bottom'>
+<div id='footer' class = 'navbar navbar-default navbar-fixed-bottom'>
     <div class = 'container'>
-        <div class='row'>
-            <div class='col-md-2'>
-                <div id='ietdstatus'>
-                    <?php if ($data[1] == 0) { ?>
-                        <a class = 'navbar-btn btn-success btn'><span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span> ietd running</a>
-                    <?php } else { ?>
-                        <a class = 'navbar-btn btn-danger btn'><span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span> ietd not running</a>
-                    <?php } ?>
+        <div class='hidden-xs hidden-sm'>
+            <div class='row'>
+                <div class='col-md-2'>
+                    <div id='ietdstatus'>
+                        <?php if ($data[1] == 0) { ?>
+                            <a class='navbar-btn btn-success btn'><span class='glyphicon glyphicon-thumbs-up' aria-hidden='true'></span> ietd running</a>
+                        <?php } else { ?>
+                            <a class='navbar-btn btn-danger btn'><span class='glyphicon glyphicon-thumbs-down' aria-hidden='true'></span> ietd not running</a>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class='col-md-offset-9 col-md-1'>
+                    <a style="font-size:10px" href='https://github.com/MrCrankHank' target='new' class='navbar-text'>Created by MrCrankHank</a>
                 </div>
             </div>
-            <div class='col-md-offset-9 col-md-1'>
-                <a style="font-size:12px" href='https://github.com/MrCrankHank' target='new' class="navbar-text">Created by MrCrankHank</a>
+        </div>
+
+        <div class='hidden-md hidden-lg'>
+            <div class='row text-center'>
+                <div class='ol-xs-12'>
+                    <div id='ietdstatus'>
+                        <?php if ($data[1] == 0) { ?>
+                            <a class='navbar-btn btn-success btn'><span class='glyphicon glyphicon-thumbs-up' aria-hidden='true'></span> ietd running</a>
+                        <?php } else { ?>
+                            <a class='navbar-btn btn-danger btn'><span class='glyphicon glyphicon-thumbs-down' aria-hidden='true'></span> ietd not running</a>
+                        <?php } ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
