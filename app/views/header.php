b720955c8b58a4255d8e4eb84741c5efbb783d1a
MrCrankHank <mail@alexander-hank.de>
2015-09-20 21:14:52 +0200

    change meta tag

--- app/views/header.php
+++ app/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/phpietadmin/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="/phpietadmin/css/bootstrap-table.css" type="text/css" rel="stylesheet">
