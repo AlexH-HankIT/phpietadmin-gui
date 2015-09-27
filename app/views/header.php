fac92bda4395380460f885fc132835eead49bc24
MrCrankHank <mail@alexander-hank.de>
2015-09-28 00:43:26 +0200

    Add bootstrap-table min css file

--- app/views/header.php
+++ app/views/header.php
@@ -3,7 +3,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="/phpietadmin/css/bootstrap.min.css" rel="stylesheet">
-<link href="/phpietadmin/css/bootstrap-table.css" rel="stylesheet">
+<link href="/phpietadmin/css/bootstrap-table.min.css" rel="stylesheet">
 <link href="/phpietadmin/css/jquery.qtip.css" rel="stylesheet">
 <link href="/phpietadmin/css/sweetalert.css" rel="stylesheet">
 <link href="/phpietadmin/css/jquery-ui.min.css" rel="stylesheet">
