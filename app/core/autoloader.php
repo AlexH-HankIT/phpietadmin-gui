7a3987bc606a142868818ccabf42db6fcf39c8f0
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-01 12:04:32 +0200

    Change include to require

--- app/core/autoloader.php
+++ app/core/autoloader.php
@@ -1,4 +1,4 @@
 <?php
     spl_autoload_register(function($class) {
-		include_once __DIR__  . '/../../../' .  str_replace('\\', '/', $class) . '.php';
+		require_once __DIR__  . '/../../../' .  str_replace('\\', '/', $class) . '.php';
 });
