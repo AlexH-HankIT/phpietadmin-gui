e2aa5eee678b6b9a0705e3cefb04a8a606c3b2f0
MrCrankHank <mail@alexander-hank.de>
2015-10-04 21:50:01 +0200

    Use filter input function

--- app/controllers/service.php
+++ app/controllers/service.php
@@ -14,16 +14,18 @@ use phpietadmin\app\core,
 
 		public function change_service_state() {
 			if (isset($_POST['servicename']) && !empty($_POST['servicename'])) {
+				$service_name = filter_input(INPUT_POST, 'servicename', FILTER_SANITIZE_STRING);
+
 				if (isset($_POST['start'])) {
-					$service = $this->model('Service', $_POST['servicename']);
+					$service = $this->model('Service', $service_name);
 					$service->action('start');
 					echo json_encode($service->logging->get_action_result());
 				} else if (isset($_POST['stop'])) {
-                    $service = $this->model('Service', $_POST['servicename']);
+                    $service = $this->model('Service', $service_name);
 					$service->action('stop');
 					echo json_encode($service->logging->get_action_result());
 				} else if (isset($_POST['restart'])) {
-                    $service = $this->model('Service', $_POST['servicename']);
+                    $service = $this->model('Service', $service_name);
 					$service->action('restart');
 					echo json_encode($service->logging->get_action_result());
 				}
@@ -32,31 +34,35 @@ use phpietadmin\app\core,
 
 		public function add() {
             if (isset($_POST['servicename'], $_POST['action']) && !$this->base_model->std->mempty($_POST['servicename'], $_POST['action'])) {
+
+				$service_name = filter_input(INPUT_POST, 'servicename', FILTER_SANITIZE_STRING);
+
                 switch($_POST['action']) {
                     case 'enable':
-                        $service = $this->model('Service', $_POST['servicename']);
+                        $service = $this->model('Service', $service_name);
                         $service->change_in_database('enable');
                         echo json_encode($service->logging->get_action_result());
                         break;
                     case 'disable';
-                        $service = $this->model('Service', $_POST['servicename']);
+                        $service = $this->model('Service', $service_name);
                         $service->change_in_database('disable');
                         echo json_encode($service->logging->get_action_result());
                         break;
                     case 'delete':
-                        $service = $this->model('Service', $_POST['servicename']);
+                        $service = $this->model('Service', $service_name);
                         $service->delete_from_db();
                         echo json_encode($service->logging->get_action_result());
                         break;
                     case 'add':
-                        $service = $this->model('Service', $_POST['servicename']);
+                        $service = $this->model('Service', $service_name);
                         $service->add_to_db();
                         echo json_encode($service->logging->get_action_result());
                         break;
                     case 'edit':
                         if (isset($_POST['newvalue'])) {
-                            $service = $this->model('Service', $_POST['servicename']);
-                            $service->rename_in_database($_POST['newvalue']);
+							$new_value = filter_input(INPUT_POST, 'newvalue', FILTER_SANITIZE_STRING);
+                            $service = $this->model('Service', $service_name);
+                            $service->rename_in_database($new_value);
                             echo json_encode($service->logging->get_action_result());
                         }
 						break;
