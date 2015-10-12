347cd5a6525412c7612644675062f1256ffe6426
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-12 13:17:53 +0200

    Add filter functions

--- app/controllers/ietusers.php
+++ app/controllers/ietusers.php
@@ -23,17 +23,23 @@ use phpietadmin\app\core;
          */
         public function add_to_db() {
             if (isset($_POST['username'], $_POST['password']) && !$this->base_model->std->mempty($_POST['username'], $_POST['password'])) {
-                $_POST['username'] = str_replace(' ', '', $_POST['username']);
-                $_POST['password'] = str_replace(' ', '', $_POST['password']);
-                $user = $this->model('Ietuser', $_POST['username']);
-                $user->add_user_to_db($_POST['password']);
+                $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+                $username = str_replace(' ', '', $username);
+                $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+                $password = str_replace(' ', '', $password);
+
+                $user = $this->model('Ietuser', $username);
+                $user->add_user_to_db($password);
                 echo json_encode($user->logging->get_action_result());
             }
         }
 
         public function delete_from_db() {
-            if (isset($_POST['username']) && !empty($_POST['username'])) {
-                $user = $this->model('Ietuser', $_POST['username']);
+            if (!empty($_POST['username'])) {
+                $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+                $username = str_replace(' ', '', $username);
+
+                $user = $this->model('Ietuser', $username);
                 $user->delete_user_from_db();
                 echo json_encode($user->logging->get_action_result());
             }
