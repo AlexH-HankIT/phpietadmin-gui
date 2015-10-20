d757dfb0152c226d867d095d5623573f671b7c71
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-10-20 15:17:08 +0200

    Remove unnecessary variable

--- app/controllers/log.php
+++ app/controllers/log.php
@@ -6,45 +6,41 @@ use phpietadmin\app\core;
             if ($param == 'action') {
                 $data = $this->base_model->std->tail($this->base_model->database->get_config('log_base')['value'] . '/' . $this->base_model->database->get_config('action_log')['value']);
                 if ($data !== false) {
-                    $view = array(
-                        'title' => 'Action',
-                        'heading' => array(
-                            'timestamp',
-                            'ip',
-                            'browser agent',
-                            'session id',
-                            'message',
-                            'status',
-                            'type',
-                            'method'
-                        ),
-                        'body' => array_reverse($data)
+                    $this->view('table', array(
+                            'title' => 'Action',
+                            'heading' => array(
+                                'timestamp',
+                                'ip',
+                                'browser agent',
+                                'message',
+                                'status',
+                                'type',
+                                'method'
+                            ),
+                            'body' => array_reverse($data)
+                        )
                     );
-
-                    $this->view('table', $view);
                 } else {
                     $this->view('message', array('message' => 'The action log file is empty!', 'type' => 'danger'));
                 }
             } else if ($param == 'access') {
                 $data = $this->base_model->std->tail($this->base_model->database->get_config('log_base')['value'] . '/' . $this->base_model->database->get_config('access_log')['value']);
                 if ($data !== false) {
-                    $view = array(
-                        'title' => 'Access',
-                        'heading' => array(
-                            'timestamp',
-                            'user',
-                            'ip',
-                            'browser agent',
-                            'session id',
-                            'message',
-                            'status',
-                            'type',
-                            'method'
-                        ),
-                        'body' => array_reverse($data)
+                    $this->view('table', array(
+                            'title' => 'Access',
+                            'heading' => array(
+                                'timestamp',
+                                'user',
+                                'ip',
+                                'browser agent',
+                                'message',
+                                'status',
+                                'type',
+                                'method'
+                            ),
+                            'body' => array_reverse($data)
+                        )
                     );
-
-                    $this->view('table', $view);
                 } else {
                     $this->view('message', array('message' => 'The access log file is empty!', 'type' => 'danger'));
                 }
