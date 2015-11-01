12c891632b495522173be27d7839a9cf73bf4c32
MrCrankHank <mail@alexander-hank.de>
2015-11-01 22:48:41 +0100

    Changes

--- app/views/config/user_table.php
+++ app/views/config/user_table.php
@@ -3,7 +3,7 @@
         <div class="panel panel-default">
             <ol class='panel-heading breadcrumb'>
                 <li><a href='#'>Config</a></li>
-                <li class='active'>Login user</li>
+                <li class='active'>User</li>
             </ol>
 
             <div class="table-responsive">
@@ -21,18 +21,10 @@
 					<?php foreach ($data as $user) { ?>
                         <tr>
                             <td><?php echo $user['username'] ?></td>
-                            <td>
-                                <input class="password1" disabled type="password" value="           "><span class="label label-success bestaetigung">Success</span>
-                            </td>
-                            <td>
-                                <input class="password2" disabled type="password" value="           "><span class="label label-success bestaetigung">Success</span>
-                            </td>
-                            <td>
-                                <a href='#'><span id="editpassword" style="font-size: 15px" class="glyphicon glyphicon-pencil"></span></a>
-                            </td>
-                            <td>
-                                <a id="savepassworda" hidden href='#'><span id="savepassword" style="font-size: 15px" class="glyphicon glyphicon-ok"></span></a>
-                            </td>
+                            <td><input class="password1" disabled type="password" value="           "><span class="label label-success bestaetigung">Success</span></td>
+                            <td><input class="password2" disabled type="password" value="           "><span class="label label-success bestaetigung">Success</span></td>
+                            <td><a href='#'><span id="editpassword" class="glyphicon glyphicon-pencil glyphicon-15"></span></a></td>
+                            <td><a id="savepassworda" hidden href='#'><span id="savepassword" class="glyphicon glyphicon-ok glyphicon-15"></span></a></td>
                         </tr>
 					<?php } ?>
                     </tbody>
