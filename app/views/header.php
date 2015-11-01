288b942e62ab22d09fae13454e9b4a9eafc2c309
MrCrankHank <mail@alexander-hank.de>
2015-11-01 20:02:41 +0100

    Change css loading order

--- app/views/header.php
+++ app/views/header.php
@@ -9,8 +9,8 @@
 <link href="/phpietadmin/css/jquery-ui.min.css" rel="stylesheet">
 <link href="/phpietadmin/css/jquery-ui-slider-pips.min.css" rel="stylesheet">
 <link href="/phpietadmin/css/jquery.bootstrap-touchspin.min.css" rel="stylesheet">
-<link href="/phpietadmin/css/my.css" rel="stylesheet">
 <link href="/phpietadmin/css/nprogress.css" rel="stylesheet">
+<link href="/phpietadmin/css/my.css" rel="stylesheet">
 <script data-main="/phpietadmin/js/common" src="/phpietadmin/js/lib/require.js"></script>
 <noscript>
     <div class="container">
