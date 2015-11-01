27f91deaf781a0ceee9466806454edf4207ad95d
MrCrankHank <mail@alexander-hank.de>
2015-11-01 22:43:25 +0100

    Optimize javascript code

--- public/js/pages/config/configuser.js
+++ public/js/pages/config/configuser.js
@@ -3,102 +3,70 @@ define(['jquery', 'mylibs', 'sweetalert'], function ($, mylibs, swal) {
 
     return methods = {
         add_event_handler_editpassword: function () {
-            var password1 = $('.password1');
-            var password2 = $('.password2');
-            var editpassword = $('#editpassword');
-            var savepasswordatag = $('#savepassworda');
+            var password1 = $('.password1'),
+                password2 = $('.password2'),
+                editpassword = $('#editpassword'),
+                savepasswordatag = $('#savepassworda');
 
-            $(document).once('click', '#editpassword', function () {
+            $('#workspace').once('click', '#editpassword', function () {
                 if (password1.prop('disabled') == true && password2.prop('disabled') == true) {
-                    password1.prop('disabled', false);
-                    password2.prop('disabled', false);
-                    password1.val('');
-                    password2.val('');
-                    password1.focus();
-                    editpassword.removeClass('glyphicon-pencil');
-                    editpassword.addClass('glyphicon-remove');
+                    password1.prop('disabled', false).val('').focus();
+                    password2.prop('disabled', false).val('');
+                    editpassword.removeClass('glyphicon-pencil').addClass('glyphicon-remove');
                     savepasswordatag.show();
                 } else {
-                    password1.prop('disabled', true);
-                    password2.prop('disabled', true);
-                    password1.val('           ');
-                    password2.val('           ');
-
-                    editpassword.removeClass('glyphicon-remove');
-                    editpassword.addClass('glyphicon-pencil');
+                    password1.prop('disabled', true).val('           ');
+                    password2.prop('disabled', true).val('           ');
+                    editpassword.removeClass('glyphicon-remove').addClass('glyphicon-pencil');
                     savepasswordatag.hide();
                 }
             });
         },
         add_event_handler_savepassword: function () {
-            var password1 = $('.password1');
-            var password2 = $('.password2');
+            var password1 = $('.password1'),
+                password2 = $('.password2'),
+                workspace = $('#workspace');
 
-            $(document).once('click', '#savepassword', function () {
-                $(document).once('focus', '.password1', function () {
-                    if (password1.hasClass("focusedInputerror")) {
-                        password1.removeClass("focusedInputerror");
-                    }
+            workspace.once('click', '#savepassword', function () {
+                workspace.once('focus', '.password1', function () {
+                    password1.removeClass("focusedInputerror");
                 });
 
-                $(document).once('focus', '.password2', function () {
-                    if (password2.hasClass("focusedInputerror")) {
-                        password2.removeClass("focusedInputerror");
-                    }
+                workspace.once('focus', '.password2', function () {
+                    password2.removeClass("focusedInputerror");
                 });
 
-                if (password1.val() == '') {
-                    password1.addClass("focusedInputerror");
-                    password1.next('.bestaetigung').addClass("label-danger");
-                    password1.next('.bestaetigung').text("Required");
-                    password1.next('.bestaetigung').show(500);
-                    password1.next('.bestaetigung').delay(2000).hide(0);
+                if (password1.val() === '') {
+                    password1.addClass("focusedInputerror").addClass("label-danger").text("Required").show(500).delay(2000).hide(0);
                 } else if (password2.val() == '') {
-                    password2.addClass("focusedInputerror");
-                    password2.next('.bestaetigung').addClass("label-danger");
-                    password2.next('.bestaetigung').text("Required");
-                    password2.next('.bestaetigung').show(500);
-                    password2.next('.bestaetigung').delay(2000).hide(0);
+                    password2.addClass("focusedInputerror").addClass("label-danger").text("Required").show(500).delay(2000).hide(0);
                 } else {
                     // Check if password1 and password2 matches
-                    if (password1.val() == password2.val()) {
-                        // Create sha256 hash from password
-                        var pwhash = sha256.hash(password1.val());
-
-                        // Ajax password to server
-                        var data = {
-                            "pwhash": pwhash
-                        };
-
-                        var request = mylibs.doajax('/phpietadmin/config/editloginuser', data);
-
-                        request.done(function () {
-                            if (request.readyState == 4 && request.status == 200) {
-                                if (request.responseText == "Success") {
-                                    // In case of success, reload the page for relogin
-                                    location.reload();
-                                } else {
-                                    swal({
-                                        title: 'Error',
-                                        type: 'error',
-                                        text: request.responseText
-                                    });
-                                }
+                    if (password1.val() === password2.val()) {
+                        $.ajax({
+                            url: '/phpietadmin/config/user/change',
+                            data: {
+                                "username": "",
+                                "row": "password",
+                                "value": password1.val()
+                            },
+                            dataType: 'json',
+                            type: 'post',
+                            success: function (data) {
+                                // fetch page update via ajax
+                            },
+                            error: function () {
+                                swal({
+                                    title: 'Error',
+                                    type: 'error',
+                                    text: 'Something went wrong while submitting!'
+                                });
                             }
                         });
                     } else {
                         // Display bad message here
-                        password1.addClass("focusedInputerror");
-                        password1.next('.bestaetigung').addClass("label-danger");
-                        password1.next('.bestaetigung').text("Doesn't match");
-                        password1.next('.bestaetigung').show(500);
-                        password1.next('.bestaetigung').delay(2000).hide(0);
-
-                        password2.addClass("focusedInputerror");
-                        password2.next('.bestaetigung').addClass("label-danger");
-                        password2.next('.bestaetigung').text("Doesn't match");
-                        password2.next('.bestaetigung').show(500);
-                        password2.next('.bestaetigung').delay(2000).hide(0);
+                        password1.addClass("focusedInputerror").addClass("label-danger").text("Doesn't match").show(500).delay(2000).hide(0);
+                        password2.addClass("focusedInputerror").addClass("label-danger").text("Doesn't match").show(500).delay(2000).hide(0);
                     }
                 }
             });
