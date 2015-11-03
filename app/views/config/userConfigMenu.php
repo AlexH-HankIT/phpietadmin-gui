6a3ce479c475a069e2ea7f1594b7bffeb8202487
mrcrankhank <alexander.hank@lra-aic-fdb.de>
2015-11-03 20:15:11 +0100

    Add userConfigMenu

--- app/views/config/userConfigMenu.php
+++ app/views/config/userConfigMenu.php
@@ -14,11 +14,12 @@
                             <h3 class="modal-title" id="myModalLabel">Edit password</h3>
                         </div>
                         <div class="modal-body">
+                            <input hidden id="savedUsername">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="input-group">
                                         <span class="input-group-addon input-group-addon-25">New password</span>
-                                        <input type="password" class="form-control" id="inputPassword" placeholder="New password...">
+                                        <input type="password" class="form-control passwordInput" id="inputPassword" placeholder="New password...">
                                     </div>
                                 </div>
                             </div>
@@ -26,34 +27,86 @@
                                 <div class="col-md-12">
                                     <div class="input-group">
                                         <span class="input-group-addon input-group-addon-25">Repeat password</span>
-                                        <input type="password" class="form-control" id="inputPasswordRepeat" placeholder="Repeat password...">
+                                        <input type="password" class="form-control passwordInput" id="inputPasswordRepeat" placeholder="Repeat password...">
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Close</button>
-                            <button type="button" class="btn btn-success" id="savePasswordButton"><span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save</button>
+                            <div class="row">
+                                <div id="showErrorInModal" class="col-md-5"></div>
+
+                                <div class="col-md-5 col-md-offset-2">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Close</button>
+                                    <button type="button" class="btn btn-success" id="savePasswordButton"><span class="glyphicon glyphicon-save" aria-hidden="true"></span> Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="createUserModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h3 class="modal-title" id="myModalLabel">Create user</h3>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <span class="input-group-addon input-group-addon-25">Username</span>
+                                        <input type="text" class="form-control" id="usernameNew" placeholder="Username...">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row top-buffer">
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <span class="input-group-addon input-group-addon-25">Password</span>
+                                        <input type="password" class="passwordInputCreateUser form-control" id="inputPasswordNew" placeholder="Password...">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row top-buffer">
+                                <div class="col-md-12">
+                                    <div class="input-group">
+                                        <span class="input-group-addon input-group-addon-25">Repeat password</span>
+                                        <input type="password" class="passwordInputCreateUser form-control" id="inputPasswordRepeatNew" placeholder="Repeat password...">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="row">
+                                <div id="showErrorInCreateUserModal" class="col-md-5"></div>
+                                <div class="col-md-5 col-md-offset-2">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> Close</button>
+                                    <button type="button" class="btn btn-success" id="saveUserButton"><span class="glyphicon glyphicon-save" aria-hidden="true"></span> Create</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="table-responsive">
-                <table class="table white-table">
+                <table class="table white-table" id="userTable">
                     <thead>
                         <tr>
-                            <th>Username</th>
-                            <th>Edit password</th>
-                            <th>Delete user</th>
+                            <th class="col-md-10">Username</th>
+                            <th class="col-md-1"><button id="addRow" class="btn btn-xs btn-success" data-toggle="modal" data-target="#createUserModal"><span class="glyphicon glyphicon-plus"></span> Add</button></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
 					<?php foreach ($data as $user) { ?>
                         <tr>
-                            <td><?php echo $user['username'] ?></td>
-                            <td><a href='#'><span class="glyphicon glyphicon-pencil glyphicon-15" data-toggle="modal" data-target="#editPasswordModal"></span></a></td>
-                            <td><a href='#'><span class="glyphicon glyphicon-remove glyphicon-15"></span></a></td>
+                            <td class="col-md-10 username"><?php echo $user['username'] ?></td>
+                            <td class="col-md-1"><button class="btn btn-xs btn-warning editPasswordSpan" data-toggle="modal" data-target="#editPasswordModal"><span class="glyphicon glyphicon-pencil"></span> Edit</button></td>
+                            <td class="col-md-1"><button class="btn btn-xs btn-danger deleteUserSpan"><span class="glyphicon glyphicon-remove"></span> Delete</button></td>
                         </tr>
 					<?php } ?>
                     </tbody>
@@ -66,7 +119,8 @@
         require(['common'],function() {
             require(['pages/config/userConfigMenu', 'domReady'], function(methods, domReady) {
 				domReady(function () {
-                    methods.model();
+                    methods.editPasswordModel();
+                    methods.addUserModal();
                     methods.table();
             	});
             });
--- public/js/pages/config/userConfigMenu.js
+++ public/js/pages/config/userConfigMenu.js
@@ -1,52 +1,169 @@
-define(['jquery', 'mylibs', 'sweetalert'], function ($, mylibs, swal) {
+define(['jquery', 'sweetalert', 'mylibs'], function ($, swal, mylibs) {
     var methods;
 
     return methods = {
-        model: function() {
+        editPasswordModel: function() {
             var editPasswordModal = $('#editPasswordModal'),
-                formControl = $('.form-control');
+                passwordInput = $('.passwordInput'),
+                inputPassword = $('#inputPassword');
 
             editPasswordModal.once('shown.bs.modal', function () {
-                $('#inputPassword').focus();
-            });
-
-            editPasswordModal.once('hidden.bs.modal', function () {
-                console.log("hide");
+                inputPassword.focus();
             });
 
             // Remove error, when input field is filled
-            formControl.once('click', function() {
+            passwordInput.once('click', function() {
                 $(this).parent('div').removeClass('has-error');
             });
 
-            $('#savePasswordButton').once('click', function() {
-                var inputPasswordVal = $('#inputPassword').val(),
-                    inputPasswordRepeatVal = $('#inputPasswordRepeat').val(),
-                    formControlParentDiv = formControl.parent('div');
+            // insert username into loaded modal
+            $('.editPasswordSpan').once('click', function() {
+                $('#savedUsername').val($(this).closest('tr').find('.username').text());
+            });
 
-                if (formControl.val() === '' || inputPasswordVal !== inputPasswordRepeatVal) {
-                    formControlParentDiv.addClass('has-error');
+            $('#savePasswordButton').once('click', function() {
+                var inputPasswordVal = inputPassword.val(),
+                    inputPasswordRepeatVal = $('#inputPasswordRepeat').val(),
+                    passwordInputParentDiv = passwordInput.parent('div');
+
+                if (passwordInput.val() === '' || inputPasswordVal !== inputPasswordRepeatVal) {
+                    passwordInputParentDiv.addClass('has-error');
                 } else {
-                    formControlParentDiv.addClass('has-success');
+                    passwordInputParentDiv.removeClass('has-error').addClass('has-success');
                 }
 
                 // Only close modal on success
-                if (formControlParentDiv.hasClass('has-success')) {
-                    // Wait a bit to inform the user of the success
-                    setTimeout(function() {
-                        editPasswordModal.modal('hide');
+                if (passwordInputParentDiv.hasClass('has-success')) {
+                    $.ajax({
+                        url: '/phpietadmin/config/user/change',
+                        data: {
+                            "username": $('#savedUsername').val(),
+                            "value": inputPasswordVal
+                        },
+                        dataType: 'json',
+                        type: 'post',
+                        success: function (data) {
+                            if (data['code'] === 0) {
+                                // Wait a bit to inform the user of the success
+                                setTimeout(function() {
+                                    editPasswordModal.modal('hide');
 
-                        // Remove success class, otherwise it is still displayed, if the user opens the modal again
-                        formControlParentDiv.removeClass('has-success');
+                                    // Remove success class, otherwise it is still displayed, if the user opens the modal again
+                                    passwordInputParentDiv.removeClass('has-success has-error');
 
-                        // Empty the password input fields
-                        formControl.val('');
-                    }, 400);
+                                    // Empty the password input fields
+                                    passwordInput.val('');
+                                }, 400);
+                            } else {
+                                $('#showErrorInModal').html(data['message']);
+                            }
+                        },
+                        error: function () {
+                            $('#showErrorInModal').html('Submit failed!');
+                        }
+                    });
+                }
+            });
+        },
+        addUserModal: function() {
+            var createUserModal = $('#createUserModal'),
+                usernameNew = $('#usernameNew'),
+                showErrorInCreateUserModal = $('#showErrorInCreateUserModal'),
+                url = '/phpietadmin/config/user',
+                passwordInputCreateUser = $('.passwordInputCreateUser'),
+                inputPasswordNewVal = $('#inputPasswordNew').val(),
+                inputPasswordRepeatNewVal = $('#inputPasswordRepeatNew').val(),
+                passwordInputCreateUserParentDiv = passwordInputCreateUser.parent('div');
+
+            createUserModal.once('shown.bs.modal', function () {
+                usernameNew.focus();
+            });
+
+            // Remove error, when input field is filled
+            passwordInputCreateUser.once('click', function() {
+                passwordInputCreateUserParentDiv.removeClass('has-error');
+            });
+
+            $('#saveUserButton').once('click', function() {
+                if (passwordInputCreateUser.val() === '' || inputPasswordNewVal !== inputPasswordRepeatNewVal) {
+                    passwordInputCreateUserParentDiv.addClass('has-error');
+                } else {
+                    passwordInputCreateUserParentDiv.removeClass('has-error').addClass('has-success');
+                }
+
+                // Only close modal on success
+                if (passwordInputCreateUserParentDiv.hasClass('has-success')) {
+                    $.ajax({
+                        url: url + '/add',
+                        data: {
+                            "username": usernameNew.val(),
+                            "password": inputPasswordNewVal
+                        },
+                        dataType: 'json',
+                        type: 'post',
+                        success: function (data) {
+                            if (data['code'] === 0) {
+                                // Wait a bit to inform the user of the success
+                                setTimeout(function() {
+                                    createUserModal.modal('hide');
+                                    // Remove success class, otherwise it is still displayed, if the user opens the modal again
+                                    //passwordInputParentDiv.removeClass('has-success has-error');
+
+                                    // Empty the password input fields
+                                    //passwordInput.val('');
+                                }, 400);
+
+                                createUserModal.once('hidden.bs.modal', function() {
+                                    return mylibs.load_workspace(url);
+                                });
+                            } else {
+                                showErrorInCreateUserModal.html(data['message']);
+                            }
+                        },
+                        error: function () {
+                            showErrorInCreateUserModal.html('Submit failed!');
+                        }
+                    });
                 }
             });
         },
         table: function() {
-
+            // delete user
+            $('#userTable').once('click', '.deleteUserSpan', function() {
+                var url = '/phpietadmin/config/user';
+                $.ajax({
+                    url: url + '/delete',
+                    data: {
+                        "username": $(this).closest('tr').find('.username').text()
+                    },
+                    dataType: 'json',
+                    type: 'post',
+                    success: function (data) {
+                        if (data['code'] === 0) {
+                            swal({
+                                title: 'Success',
+                                type: 'success',
+                                text: data['message']
+                            }, function () {
+                                return mylibs.load_workspace(url);
+                            });
+                        } else {
+                            swal({
+                                title: 'Error',
+                                type: 'error',
+                                text: data['message']
+                            });
+                        }
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Error',
+                            type: 'error',
+                            text: 'Something went wrong while submitting!'
+                        });
+                    }
+                });
+            });
         }
     };
 });
