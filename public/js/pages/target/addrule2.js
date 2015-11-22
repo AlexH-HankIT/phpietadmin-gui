1864dac3d871871c7089f8658c8f4e29cc444cf1
MrCrankHank <mail@alexander-hank.de>
2015-11-22 04:13:54 +0100

    Rename

---
+++ public/js/pages/target/addrule2.js
@@ -0,0 +1,64 @@
+define(['jquery', 'mylibs', 'sweetalert'], function ($, mylibs, swal) {
+    var methods;
+    return methods = {
+        enable_filter_table_plugin: function () {
+            // Enable filter table plugin
+            $('.searchabletable').filterTable({minRows: 0});
+
+        },
+        add_event_handler_addallowrulebutton: function () {
+            mylibs.select_all_checkbox($('#masterCheckbox'));
+
+            $('#addAllowRuleButton').once('click', function () {
+                var checkboxes = $('.objectCheckbox:checked');
+
+                if (!checkboxes.val()) {
+                    swal({
+                        title: 'Error',
+                        type: 'error',
+                        text: 'Please select a object!'
+                    });
+                } else {
+                    checkboxes.each(function () {
+                        var $this = $(this);
+
+                        $.ajax({
+                            url: '/phpietadmin/targets/configure/' + $('#targetSelect').find('option:selected').val() + '/addrule',
+                            data: {
+                                'type': $("input[name='type']:checked").val(),
+                                'id': $this.closest('tr').find('.objectId').text()
+                            },
+                            dataType: 'json',
+                            type: 'post',
+                            success: function (data) {
+                                if (data['code'] === 0) {
+                                    swal({
+                                        title: 'Success',
+                                        type: 'success',
+                                        text: data['message']
+                                    }, function() {
+                                        // uncheck all the checkbox
+                                        $this.removeAttr('checked');
+                                    });
+                                } else {
+                                    swal({
+                                        title: 'Error',
+                                        type: 'error',
+                                        text: data['message']
+                                    });
+                                }
+                            },
+                            error: function () {
+                                swal({
+                                    title: 'Error',
+                                    type: 'error',
+                                    text: 'Something went wrong while submitting!'
+                                });
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    };
+});
