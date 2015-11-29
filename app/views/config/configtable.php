1c74de3cc4b5d9679f156506b423254b31aef4cf
MrCrankHank <mail@alexander-hank.de>
2015-11-29 17:34:46 +0100

    Add select

--- app/views/config/configtable.php
+++ app/views/config/configtable.php
@@ -19,8 +19,22 @@
                             <tr>
                                 <td><?php echo htmlspecialchars($value['optioningui']); ?></td>
                                 <td>
-                                    <input size="80" type="text" name="fname" value="<?php echo htmlspecialchars($value['value']); ?>" disabled>
-                                    <a href="#<?php echo htmlspecialchars($value['option']); ?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                                    <?php if($value['field'] === 'input') { ?>
+                                        <input size="80" type="text" name="fname" value="<?php echo htmlspecialchars($value['value']); ?>" disabled>
+                                        <a href="#<?php echo htmlspecialchars($value['option']); ?>"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                                    <?php } else if ($value['field'] === 'select') { ?>
+                                        <?php if ($value['type'] === 'bool') { ?>
+                                            <select>
+                                                <?php if($value['value'] === 0) { ?>
+                                                    <option>Disabled</option>
+                                                    <option>Enabled</option>
+                                                <?php } else if ($value['value'] === 1) { ?>
+                                                    <option>Enabled</option>
+                                                    <option>Disabled</option>
+                                                <?php } ?>
+                                            </select>
+                                        <?php } ?>
+                                    <?php } ?>
                                     <span class="label label-success bestaetigung">Success</span>
                                 </td>
                                 <td><?php if (isset($value['description'])) echo htmlspecialchars($value['description']) ?></td>
