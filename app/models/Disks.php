68f6c552dbd315f5869a0b827104184c3ac0d499
MrCrankHank <mail@alexander-hank.de>
2015-12-13 20:26:12 +0100

    Bug fixes

--- app/models/Disks.php
+++ app/models/Disks.php
@@ -1,50 +1,49 @@
-<?php 
+<?php
 
-namespace phpietadmin\app\models;
-    
-use phpietadmin\app\models\logging,
-		phpietadmin\app\core;
+namespace app\models;
 
-    class Disks extends core\BaseModel {
+use app\models\logging,
+    app\core;
 
-        public function get_disks() {
-            $return = $this->std->exec_and_return($this->database->get_config('lsblk')['value'] . ' --pairs');
+class Disks extends core\BaseModel {
+    public function get_disks($format = 'array') {
+        $return = $this->std->exec_and_return($this->database->get_config('lsblk')['value'] . ' --pairs');
 
-            if ($return['result'] == 0) {
-                if (!empty($return['status'])) {
-                    foreach ($return['status'] as $key => $data) {
-                        preg_match_all('/(\w+)\s*=\s*(["\'])((?:(?!\2).)*)\2/', $data, $temp, PREG_SET_ORDER);
+        if ($return['result'] == 0) {
+            if (!empty($return['status'])) {
+                foreach ($return['status'] as $key => $data) {
+                    preg_match_all('/(\w+)\s*=\s*(["\'])((?:(?!\2).)*)\2/', $data, $temp, PREG_SET_ORDER);
 
-                        foreach ($temp as $value) {
-                            $disks[$key][$value[1]] = $value[3];
-                        }
+                    foreach ($temp as $value) {
+                        $disks[$key][$value[1]] = $value[3];
                     }
+                }
 
-                    array_walk($disks, function(&$value) {
-                        if ($value['TYPE'] == 'lvm') {
-                            // unset doesn't work with references
-                            // instead we set the value to NULL
-                            // which will remove the variable and unset its value
-                            $value = NULL;
-                        }
-                    });
-
-                    $data = array(
-                        'title' => 'Disks',
-                        'heading' => array_keys($disks[0]),
-                        'body' => array_values(array_filter($disks))
-                    );
-
-                    if ($format === 'json') {
-                        return json_encode($data);
-                    } else {
-                        return $data;
+                array_walk($disks, function (&$value) {
+                    if ($value['TYPE'] == 'lvm') {
+                        // unset doesn't work with references
+                        // instead we set the value to NULL
+                        // which will remove the variable and unset its value
+                        $value = NULL;
                     }
+                });
+
+                $data = array(
+                    'title' => 'Disks',
+                    'heading' => array_keys($disks[0]),
+                    'body' => array_values(array_filter($disks))
+                );
+
+                if ($format === 'json') {
+                    return json_encode($data);
                 } else {
-                    return false;
+                    return $data;
                 }
             } else {
                 return false;
             }
+        } else {
+            return false;
         }
     }
+}
