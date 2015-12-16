d0a047c5960c060c721d89a37814ad585fad6177
mrcrankhank <mail@alexnader-hank.de>
2015-12-16 08:27:44 +0100

    user

--- app/controllers/install.php
+++ app/controllers/install.php
@@ -11,10 +11,13 @@ class Install extends core\BaseController {
         // if database exists throw exception for security reasons
         // also if status="installed"
         $user = $this->model('User', false);
+        $versionFile = $this->baseModel->std->getVersionFile();
 
         if (file_exists($this->db_temp)) {
             throw new \Exception();
         } else if ($user->returnData() !== false) {
+            throw new \Exception();
+        } else if ($versionFile['status'] !== 'new') {
             throw new \Exception();
         }
     }
@@ -35,12 +38,18 @@ class Install extends core\BaseController {
     }
 
     public function user() {
-        $user = $this->model('User', false);
-        var_dump($user->returnData());
-
         // create first user
         // echo path to script for auth code generation
         // provide help in case of error
         // set version file status="installed"
+        if (isset($_POST['password1'], $_POST['password2'], $_POST['authCode'], $_POST['username'])) {
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+            $authCode = filter_input(INPUT_POST, 'authCode', FILTER_SANITIZE_STRING);
+            $password1 = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_STRING);
+            $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_STRING);
+            $user = $this->model('User', $username);
+            $user->addFirstUser($authCode, $password1, $password2);
+            echo json_encode($user->logging->get_action_result());
+        }
     }
 }
