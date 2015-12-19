348f2688dee77f193e5cc4ea5c4ec171a65ecc3e
MrCrankHank <mail@alexander-hank.de>
2015-12-19 21:53:28 +0100

    rename ietusers

---
+++ app/controllers/Ietusers2.php
@@ -0,0 +1,49 @@
+<?php
+namespace app\controllers;
+
+use app\core;
+
+class Ietusers extends core\BaseController {
+    /**
+     *
+     * Display table with users
+     *
+     * @return      void
+     *
+     */
+    public function index() {
+        // get all all username, passwords and ids from database and turn them over to the view
+        $this->view('ietUserTable', $this->baseModel->database->get_all_users());
+    }
+
+    /**
+     *
+     * Adds a user to the database
+     *
+     * @return      void
+     *
+     */
+    public function add_to_db() {
+        if (isset($_POST['username'], $_POST['password']) && !$this->baseModel->std->mempty($_POST['username'], $_POST['password'])) {
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+            $username = str_replace(' ', '', $username);
+            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+            $password = str_replace(' ', '', $password);
+
+            $user = $this->model('Ietuser', $username);
+            $user->add_user_to_db($password);
+            echo json_encode($user->logging->get_action_result());
+        }
+    }
+
+    public function delete_from_db() {
+        if (!empty($_POST['username'])) {
+            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+            $username = str_replace(' ', '', $username);
+
+            $user = $this->model('Ietuser', $username);
+            $user->delete_user_from_db();
+            echo json_encode($user->logging->get_action_result());
+        }
+    }
+}
