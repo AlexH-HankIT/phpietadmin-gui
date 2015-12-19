59bfaa38eb3a1a62ef85088bf1cdc312523e76e2
MrCrankHank <mail@alexander-hank.de>
2015-12-20 00:27:02 +0100

    Remove firstSign function; rename login function

--- app/views/login.php
+++ app/views/login.php
@@ -26,7 +26,7 @@
     require(['common'], function () {
         require(['pages/login', 'domReady'], function (methods, domReady) {
             domReady(function () {
-                methods.signIn();
+                methods.login();
             });
         });
     });
