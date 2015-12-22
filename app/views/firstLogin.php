3e32f54ea7425eb502d2c97220f8883521807bc4
mrcrankhank <mail@alexander-hank.de>
2015-12-22 17:00:19 +0100

    Add firstLogin.php

---
+++ app/views/firstLogin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="<?php echo WEB_PATH ?>/css/bootstrap.css" type="text/css" rel="stylesheet">
+<link href="<?php echo WEB_PATH ?>/css/base.css" type="text/css" rel="stylesheet">
+<link href="<?php echo WEB_PATH ?>/css/login.css" type="text/css" rel="stylesheet">
+<script data-main="<?php echo WEB_PATH ?>/js/common" src="<?php echo WEB_PATH ?>/js/lib/require.js"></script>
+<title>phpietadmin first login</title>
+<div class="container">
+    <form method="post" class="form-signin" autocomplete="off">
+        <h2 class="form-signin-header">phpietadmin</h2>
+        <div class="form-group">
+            <input type="text" name="authode" class="form-control" placeholder="Auth code" required autofocus>
+        </div>
+        <div class="form-group">
+            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+        </div>
+        <div class="form-group">
+            <input type="password" name="password1" class="form-control" placeholder="Password" required>
+        </div>
+        <div class="form-group">
+            <input type="password" name="password2" class="form-control" placeholder="Repeat" required>
+        </div>
+        <input class="btn btn-lg btn-primary btn-block button-width" type='submit' value='Login'>
+        <div class="form-group formError"></div>
+    </form>
+</div>
+
+<script>
+    require(['common'], function () {
+        require(['pages/firstLogin', 'domReady'], function (methods, domReady) {
+            domReady(function () {
+                methods.firstLogin();
+            });
+        });
+    });
+</script>
