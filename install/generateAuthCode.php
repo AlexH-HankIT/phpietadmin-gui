3f305becac35bf8081a16bbc46f71b5f604fdd77
mrcrankhank <mail@alexander-hank.de>
2015-12-30 07:08:17 +0100

    Add dialog

--- install/generateAuthCode.php
+++ install/generateAuthCode.php
@@ -7,14 +7,36 @@ function random_password( $length = 8 ) {
 	return $password;
 }
 
+function dialog ($args) {
+	$pipes = array (NULL, NULL, NULL);
+	// Allow user to interact with dialog
+	$in = fopen ('php://stdin', 'r');
+	$out = fopen ('php://stdout', 'w');
+	// But tell PHP to redirect stderr so we can read it
+	$p = proc_open ('dialog '.$args, array (
+		0 => $in,
+		1 => $out,
+		2 => array ('pipe', 'w')
+	), $pipes);
+	// Wait for and read result
+	$result = stream_get_contents ($pipes[2]);
+	// Close all handles
+	fclose ($pipes[2]);
+	fclose ($out);
+	fclose ($in);
+	proc_close ($p);
+	// Return result
+	return $result;
+}
+
 if (file_exists(AUTH_FILE)) {
-	echo "Code was already generated!\n";
+	dialog('--title "Auth code" --msgbox "\nAuth code was already generated: "' . file_get_contents(AUTH_FILE) . ' 14 60');
 } else {
 	$password = random_password(8);
 	if (file_put_contents(AUTH_FILE, $password) !== false) {
-		echo "Code is " . $password . "\nOpen your new phpietadmin installation and create a user.\n";
+		dialog('--title "Auth code" --msgbox "\nThanks for installing phpietadmin.\n\nThis is your auth code: ' . $password . '\n\nPress OK and copy it to your clipboard.\n\nThen start your favorite browser and open http://$(hostname -f)/phpietadmin" 14 60');
 		chown(AUTH_FILE, 'www-data');
 	} else {
-		echo "Failure\n";
+		dialog('--title "Auth code" --msgbox "\nThanks for installing phpietadmin.\n\nCan\'t generate an auth code. Please check the permissions and open an issue on github, if the error is persistent." 14 60');
 	}
 }
