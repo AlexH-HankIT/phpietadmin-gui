b1164a1a10ad7af1e9b731e248f66c6089049b5d
MrCrankHank <mail@alexander-hank.de>
2016-01-02 00:44:40 +0100

    Fix issue with message template

--- app/views/message.php
+++ app/views/message.php
@@ -1,24 +1,25 @@
 <div class="workspace">
-    <?php if (is_array($data)) { ?>
-        <?php if (!empty($data['type'])) { ?>
-            <?php if ($data['type'] == 'success') { ?>
-                <div class="alert alert-success" role="alert"><h3
-                        class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
-            <?php } else if ($data['type'] == 'warning') { ?>
-                <div class="alert alert-warning" role="alert"><h3
-                        class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
-            <?php } else if ($data['type'] == 'danger') { ?>
-                <div class="alert alert-danger" role="alert"><h3
-                        class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+    <?php if (!isset($data['container'])) { ?>
+    <div class="container">
+        <?php } ?>
+        <?php if (is_array($data)) { ?>
+            <?php if (!empty($data['type'])) { ?>
+                <?php if ($data['type'] == 'success') { ?>
+                    <div class="alert alert-success" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+                <?php } else if ($data['type'] == 'warning') { ?>
+                    <div class="alert alert-warning" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+                <?php } else if ($data['type'] == 'danger') { ?>
+                    <div class="alert alert-danger" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+                <?php } else { ?>
+                    <div class="alert alert-info" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+                <?php } ?>
             <?php } else { ?>
-                <div class="alert alert-info" role="alert"><h3
-                        class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+                <div class="alert alert-info" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
             <?php } ?>
         <?php } else { ?>
-            <div class="alert alert-info" role="alert"><h3
-                    class="text-center"><?php echo htmlspecialchars($data['message']); ?></h3></div>
+            <div class="alert alert-info" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data); ?></h3></div>
         <?php } ?>
-    <?php } else { ?>
-        <div class="alert alert-info" role="alert"><h3 class="text-center"><?php echo htmlspecialchars($data); ?></h3></div>
-    <?php } ?>
+        <?php if (!isset($data['container'])) { ?>
+    </div>
+<?php } ?>
 </div>
